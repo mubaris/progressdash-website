384d4f34a1c747c89c1bbcde1fc3f15fdb0a8256
Mubaris NK <hello@mubaris.com>
2019-03-04 11:51:37 +0530

    Add cors

--- functions/newuser.js
+++ functions/newuser.js
@@ -1,16 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 exports.handler = async (event, context) => {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+    };
     const token = process.env.TELEGRAM_TOKEN;
     const bot = new TelegramBot(token);
     const chatId = process.env.CHAT_ID;
     return bot.sendMessage(chatId, "New user! YAY! 🎉🤑💸")
         .then(() => ({
             statusCode: 200,
+            headers: headers,
             body: `Message has been sent to Telegram 🤗`
         }))
         .catch(error => ({
             statusCode: 422,
+            headers: headers,
             body: `Oops! Something went wrong. ${error}`
         }));
 };
