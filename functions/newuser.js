ac0a0daaf147999422a7263fef5b243cc9a881a2
Mubaris NK <hello@mubaris.com>
2019-03-04 10:50:29 +0530

    Fallback to require statements

--- functions/newuser.js
+++ functions/newuser.js
@@ -1,4 +1,4 @@
-import TelegramBot from 'node-telegram-bot-api';
+const TelegramBot = require('node-telegram-bot-api');
 
 exports.handler = async (event, context) => {
     const token = process.env.TELEGRAM_TOKEN;
